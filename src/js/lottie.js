3ce5e5c7478f296428d7f430a6f9ee1707d77470
Imran <mahmud.bubt.150@gmail.com>
2021-11-04 05:12:51 +0600

    Added All files

---
+++ src/js/lottie.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets9.lottiefiles.com/packages/lf20_wc7xu5jm/lap.json' // lottie file path
+    })
